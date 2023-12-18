5057274ef38b6a0a4ee54a50e81da543d4b2a5ac
Ayisha-SS <13aiysh246@gmail.com>
2023-12-18 09:15:01 +0530

    'update'

--- src/Components/Home/Earbuds/ViewAll/Sort.jsx
+++ src/Components/Home/Earbuds/ViewAll/Sort.jsx
@@ -7,17 +7,17 @@ function Sort() {
 
   return (
     <div className='pt-5'>
-        <button onClick={() => setState(true)} className='flex text-lg font-bold  border-black px-4 py-2 rounded-md bg-[#eff4f7]'><BiSortAlt2 />Sort by <MdKeyboardArrowDown /></button>
+        <button onClick={() => setState(true)} className='relative flex text-lg font-bold  border-black px-4 py-2 rounded-md bg-[#eff4f7]'><BiSortAlt2 />Sort by <MdKeyboardArrowDown /></button>
         {state && (
-          <div className='px-3 py-3 bg-slate-100 outline z-10'>
-            <ul>
-              <li>Featured</li>
-              <li>Best selling</li>
-              <li>Alphabetically,A-Z</li>
-              <li>Alphabetically,Z-A</li>
-              <li>Price,low to high</li>
-              <li>Price,high to low</li>
-              <li>New Arrivals</li>
+          <div className=' absolute pl-8 pr-10 bg-slate-100 border-solid z-10 right-4 mt-2'>
+            <ul className='my-4 pr-6'>
+              <li className='mb-3 tracking-widest mr-5 text-base font-thin'>Featured</li>
+              <li className='mb-3 tracking-widest mr-5 text-base font-thin'>Best selling</li>
+              <li className='mb-3 tracking-widest mr-5 text-base font-thin'>Alphabetically,A-Z</li>
+              <li className='mb-3 tracking-widest mr-5 text-base font-thin'>Alphabetically,Z-A</li>
+              <li className='mb-3 tracking-widest mr-5 text-base font-thin'>Price,low to high</li>
+              <li className='mb-3 tracking-widest mr-5 text-base font-thin'>Price,high to low</li>
+              <li className='mb-3 tracking-widest mr-5 text-base font-thin'>New Arrivals</li>
             </ul>
           </div>
         )}
