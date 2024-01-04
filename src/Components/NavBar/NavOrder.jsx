08baa768a35499e3ad8830ac4fa4783aa2c140f1
Ayisha-SS <13aiysh246@gmail.com>
2024-01-04 18:00:46 +0530

    update

--- src/Components/NavBar/NavOrder.jsx
+++ src/Components/NavBar/NavOrder.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function NavOrder() {
   return (
     <>
-        <div className='p-3 bg-[#eff4f7]'>
-            <h4 className='text-center text-xs cursor-pointer font-thin'>Swish! Game like a Ninja with <span className='font-extrabold'>Immortal Katana Blade.</span> Pre-order & get <span className='font-extrabold'>FREE engraving.</span></h4>
+        <div className='p-3 bg-[#eff4f7] tracking-wider'>
+            <h3 className='text-center text-xs cursor-pointer font-thin'>All-new <span className='font-extrabold'>Nirvana ION ANC</span> is out now with up to <span className='font-extrabold'>32dB noise cancellation</span></h3>
         </div>
     </>
   )
