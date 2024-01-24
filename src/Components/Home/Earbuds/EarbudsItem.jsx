a74aef17cdf0ec328778152da2ac7f7544e05daa
Ayisha-SS <13aiysh246@gmail.com>
2024-01-24 18:02:55 +0530

    update

--- src/Components/Home/Earbuds/EarbudsItem.jsx
+++ src/Components/Home/Earbuds/EarbudsItem.jsx
@@ -5,9 +5,9 @@ import {Link} from "react-router-dom"
 import PopUp from "./PopUp/PopUp"
 import { Cart } from '../../Context/Context';
 
-function EarbudsItem({item,searchTerm,setTerm}) {
+function EarbudsItem({product,searchTerm,setTerm}) {
 
-    item = earbud.earbuds
+    product = earbud.earbuds
 
     const [popup,setPopup] = useState(false)
     const [searchedItems,setSearchedItems] = useState(earbud)
@@ -25,7 +25,7 @@ function EarbudsItem({item,searchTerm,setTerm}) {
         document.body.classList.remove("overflow-y-hidden")
     }
  
-const filterProducts  = item 
+const filterProducts  = product
 .filter((item) => {
     
     if(searchTerm == undefined){
@@ -47,37 +47,28 @@ const filterProducts  = item
         
 
 {/* PRODUCT CARD... */}
-            {filterProducts
-            // .filter((item) =>{
-                
-            //     if(searchTerm == undefined){
-                    
-            //         return item;    
-            //     }else if(item.name.toLowerCase().includes(searchTerm)){
-                   
-            //         return item;
-            //     }
-            // })
-            .map((item)=> {
+            {product
+            
+            .map((items)=> {
                 return (
-                    <div key={item.id} className='w-[25%] flex flex-col relative rounded-2xl shadow-3xl'>
+                    <div key={items.id} className='w-[25%] flex flex-col relative rounded-2xl shadow-3xl'>
                         <div>
-                            <span className='absolute text-[10px] tracking-tight font-bold z-[1] left-0 top-2.5 rounded-r-lg bg-black text-white p-1'>{item.status}</span>
-                            <span className='absolute justify-center  bottom-36 z-[1] left-[15%] text-center border-1 border-solid border-black rounded-2xl py-1.5 w-[70%] text-xs font-bold bg-yellow-500 text-black tracking-tight'>{item.hours}</span>
+                            <span className='absolute text-[10px] tracking-tight font-bold z-[1] left-0 top-2.5 rounded-r-lg bg-black text-white p-1'>{items.status}</span>
+                            <span className='absolute justify-center  bottom-36 z-[1] left-[15%] text-center border-1 border-solid border-black rounded-2xl py-1.5 w-[70%] text-xs font-bold bg-yellow-500 text-black tracking-tight'>{items.hours}</span>
                         </div>
                         <div className="h-80 w-[100%] rounded-t-3x ">
-                            <img src={item.image} alt={item.name} className='w-[100%] rounded-lg'/>
+                            <img src={items.image} alt={items.name} className='w-[100%] rounded-lg'/>
                         </div>
                         <div className="px-2.5 py-6 bg-slate-100">
                             <div className=" text-4">
                                 <div className='flex justify-between'>
-                                    <h5 className='font-extrabold '>{item.name}</h5>
-                                    <p>{item.color}</p>
+                                    <h5 className='font-extrabold '>{items.name}</h5>
+                                    <p>{items.color}</p>
                                 </div>
                                 <div className='flex pt-2 gap-1 mt-1.5 mb-2.5'>
-                                    <p className='text-sm mb-1 font-bold'>&#8377;{Number(item.price).toFixed(2)}</p>
-                                    <p className='mb-1 text-xs text-gray-400 line-through'>&#8377;{item.oldPrice}</p>
-                                    <p className='text-xs pr-1.5 text-green-400'>{item.off}</p>
+                                    <p className='text-sm mb-1 font-bold'>&#8377;{Number(items.price).toFixed(2)}</p>
+                                    <p className='mb-1 text-xs text-gray-400 line-through'>&#8377;{items.oldPrice}</p>
+                                    <p className='text-xs pr-1.5 text-green-400'>{items.off}</p>
                                 </div>
                             </div>
                             <div className="">
@@ -85,7 +76,7 @@ const filterProducts  = item
                                     onClick={() =>setPopup(true)} 
                                 className='px-32 whitespace-nowrap	py-2 bg-black text-sm text-white rounded-lg'>Add To Cart</button> */}
                             
-                                <button key={item.id}
+                                <button key={items.id}
                                     onClick={()=>{
                                         handleAdd(item)}
                                     }
