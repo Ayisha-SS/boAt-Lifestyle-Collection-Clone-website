69d392e7161b870fb2014e87e61388136ede3cf6
Ayisha-SS <13aiysh246@gmail.com>
2024-02-06 14:11:04 +0530

    update

--- src/Components/NavBar/NavOrder.jsx
+++ src/Components/NavBar/NavOrder.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function NavOrder() {
   return (
-    <>
+   
         <div className='p-3 bg-[#eff4f7] tracking-wider'>
             <h3 className='text-center text-xs cursor-pointer font-thin'>All-new <span className='font-extrabold'>Nirvana ION ANC</span> is out now with up to <span className='font-extrabold'>32dB noise cancellation</span></h3>
         </div>
-    </>
+   
   )
 }
 
